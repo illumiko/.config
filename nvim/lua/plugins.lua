626836333952b7bd97d478f993321f62ea2c653b
illumiko <eyanat.rayannjr@gmail.com>
2022-03-05 22:52:09 +0600

    changed animation of miniIndentScope

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -384,7 +384,7 @@ return require('packer').startup(function(use)
         draw = {
             -- Delay (in ms) between event and start of drawing scope indicator
             delay = 0,
-
+            require("localPLUG.indentScope").gen_animation('quarticOut')
             -- Animation rule for scope's first drawing. A function which, given next
             -- and total step numbers, returns wait time (in ms). See
             -- |MiniIndentscope.gen_animation()| for builtin options. To not use
--- nvim/lua/snippets/mySnips.lua
+++ nvim/lua/snippets/mySnips.lua
@@ -1,7 +1,13 @@
 local ls = require("luasnip")
 ls.snippets = {
   norg = {
-    ls.parser.parse_snippet("ses", "- [ ] session $1 {$2} [$0->to]")
+    ls.parser.parse_snippet("ses", "- [ ] session $1 {$2} [$0->to]"),
+    ls.parser.parse_snippet("h1", "* $1"),
+    ls.parser.parse_snippet("h2", "** $1"),
+    ls.parser.parse_snippet("h3", "*** $1"),
+    ls.parser.parse_snippet("h4", "**** $1"),
+    ls.parser.parse_snippet("h5", "***** $1"),
+    ls.parser.parse_snippet("h6", "****** $1"),
   },
   lua = {
     ls.parser.parse_snippet("lvr","local $1 = require('$0')")
