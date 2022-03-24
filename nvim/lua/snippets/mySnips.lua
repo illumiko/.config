ec3854198b280fc2698510159f7e2873fd670acf
illumiko <eyanat.rayannjr@gmail.com>
2022-03-25 00:06:59 +0600

    typo fix

--- nvim/lua/snippets/mySnips.lua
+++ nvim/lua/snippets/mySnips.lua
@@ -33,11 +33,11 @@ ls.snippets = {
     ls.parser.parse_snippet("Ud", "#contexts Udvash"),
     ls.parser.parse_snippet("UdEx", "#contexts UdvashExam"),
     ls.parser.parse_snippet("UdExP", "#contexts UdvashExamPrep"),
-    ls.parser.parse_snippet("Todo", "#contexts rodo"),
+    ls.parser.parse_snippet("Todo", "#contexts Todo"),
     ls.parser.parse_snippet("code", "#contexts Code"),
     ls.parser.parse_snippet("pre", "#contexts PreTestPrep"),
     ls.parser.parse_snippet("sHw", "#contexts SchoolHw"),
-    s({trig="ses", docstring="ses"}, 
+    s({trig="ses", docstring="ses"},
     {
       t("** Session "), i(1,"1 "),
       f(function ()
