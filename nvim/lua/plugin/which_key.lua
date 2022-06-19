843761e3d7f22c4510180b20516d5148b002c2b2
illumiko <eyanat.rayannjr@gmail.com>
2022-06-19 19:37:53 +0600

    added oregairu wallpaper

--- nvim/lua/plugin/which_key.lua
+++ nvim/lua/plugin/which_key.lua
@@ -1,14 +1,14 @@
 require("which-key").setup({
 	layout = {
-		height = { min = 4, max = 25 }, -- min and max height of the columns
-		width = { min = 20, max = 50 }, -- min and max width of the columns
-		spacing = 3, -- spacing between columns
+		height = { min = 4, max = 65 }, -- min and max height of the columns
+		width = { min = 10, max = 50 }, -- min and max width of the columns
+		spacing = 1, -- spacing between columns
 		align = "left", -- align columns left, center or right
 	},
 	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
 	window = {
 		border = "single",
-		position = "top",
+		position = "bottom",
 		margin = { 1, 2, 1, 2 }, -- extra window margin [top, right, bottom, left]
 		padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
 		winblend = 0,
